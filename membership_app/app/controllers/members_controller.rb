faafee915f9effe0a0de27811d8ec6b36d92cb9a
Zack Youngren <zack@youngren.io>
2015-07-17 21:28:06 -0400

    Add some member controller actions

--- membership_app/app/controllers/members_controller.rb
+++ membership_app/app/controllers/members_controller.rb
@@ -1,10 +1,35 @@
 class MembersController < ApplicationController
-  def new
-  end
+  attr_accessor :legal_name, :derby_name, :email_address
 
   def index
+    @members = Member.all
+  end
+
+  def new
+    @member = Member.new
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+  end
+
+  def create
+    @member = Member.new(member_params)
+    if @member.save
+      flash[:success] = 'Member Created'
+      redirect_to @member
+    else
+      render 'new'
+    end
   end
 
   def show
+    @member = Member.find(params[:id])
   end
+
+  private
+
+    def member_params
+      params.require(:user).permit(:legal_name, :derby_name, :email_address)
+    end
 end
