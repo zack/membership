790db25b5a1e4337b7e24469594896ba09744bfb
Zack Youngren <zack@youngren.io>
2015-07-30 17:55:50 -0400

    Tweak model validity spec structure

--- spec/models/committee_spec.rb
+++ spec/models/committee_spec.rb
@@ -1,21 +1,25 @@
 describe Committee do
   describe 'validation' do
-    it 'is valid with a name' do
-      expect {Committee.create(name: 'Laurie')}.to change(Committee, :count).by(1)
+    context 'is valid' do
+      it 'with a name' do
+        expect {Committee.create(name: 'Laurie')}.to change(Committee, :count).by(1)
+      end
     end
 
-    it 'is invalid without a name' do
-      expect {Committee.create()}.not_to change(Committee, :count)
-    end
+    context 'invalid' do
+      it 'without a name' do
+        expect {Committee.create()}.not_to change(Committee, :count)
+      end
 
-    it 'is invalid with a duplicate name' do
-      Committee.create(name: 'Azar')
-      expect {Committee.create(name: 'Azar')}.not_to change(Committee, :count)
-    end
+      it 'with a duplicate name' do
+        Committee.create(name: 'Azar')
+        expect {Committee.create(name: 'Azar')}.not_to change(Committee, :count)
+      end
 
-    it 'ignores case with name validation' do
-      Committee.create(name: 'KERRY')
-      expect {Committee.create(name: 'kerry')}.not_to change(Committee, :count)
+      it 'with a duplicate name regardless of case' do
+        Committee.create(name: 'KERRY')
+        expect {Committee.create(name: 'kerry')}.not_to change(Committee, :count)
+      end
     end
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,16 +1,16 @@
 describe Team do
   describe 'validation' do
-    context 'valid' do
-      it 'is valid with a name' do # and no date
+    context 'is valid' do
+      it 'with a name' do # and no date
         expect {Team.create(name: 'Massacre')}.to change(Team, :count).by(1)
       end
 
-      it 'is valid with an end date and no start date' do
+      it 'with an end date and no start date' do
         expect {Team.create(name: 'Massacre', date_ended: Date.today)}.
                to change(Team, :count).by(1)
       end
 
-      it 'is valid with an end date after a start date' do
+      it 'with an end date after a start date' do
         expect {Team.create(name: 'Massacre',
                             date_started: Date.today - 1,
                             date_ended: Date.today)}.
@@ -18,22 +18,22 @@ describe Team do
       end
     end
 
-    context 'invalid' do
-      it 'is invalid without a name' do
+    context 'is invalid' do
+      it 'without a name' do
         expect {Team.create()}.not_to change(Team, :count)
       end
 
-      it 'is invalid with a duplicate name' do
+      it 'with a duplicate name' do
         Team.create!(name: 'Massacre')
         expect {Team.create(name: 'Massacre')}.not_to change(Team, :count)
       end
 
-      it 'ignores case with name validation' do
+      it 'with a duplicate name regardless of case' do
         Team.create!(name: 'MASSACRE')
         expect {Team.create(name: 'massacre')}.not_to change(Team, :count)
       end
 
-      it 'is invalid with an end date before a start date' do
+      it 'with an end date before a start date' do
         expect {Team.create(name: 'Massacre',
                             date_started: Date.today,
                             date_ended: Date.today - 1)}.
