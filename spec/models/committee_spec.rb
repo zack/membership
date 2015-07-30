51b556b4095c79cd4c5c1cfd2328b46b57d2c431
Zack Youngren <zack@youngren.io>
2015-07-30 18:39:48 -0400

    Rewrite committee specs to remove expect/change

--- spec/models/committee_spec.rb
+++ spec/models/committee_spec.rb
@@ -2,23 +2,30 @@ describe Committee do
   describe 'validation' do
     context 'is valid' do
       it 'with a name' do
-        expect {Committee.create(name: 'Laurie')}.to change(Committee, :count).by(1)
+        expect(Committee.count).to eq(0)
+        Committee.create(name: 'Laurie')
+        expect(Committee.count).to eq(1)
       end
     end
 
     context 'invalid' do
       it 'without a name' do
-        expect {Committee.create()}.not_to change(Committee, :count)
+        Committee.create()
+        expect(Committee.count).to eq(0)
       end
 
       it 'with a duplicate name' do
+        expect(Committee.count).to eq(0)
         Committee.create(name: 'Azar')
-        expect {Committee.create(name: 'Azar')}.not_to change(Committee, :count)
+        Committee.create(name: 'Azar')
+        expect(Committee.count).to eq(1)
       end
 
       it 'with a duplicate name regardless of case' do
+        expect(Committee.count).to eq(0)
+        Committee.create(name: 'kerry')
         Committee.create(name: 'KERRY')
-        expect {Committee.create(name: 'kerry')}.not_to change(Committee, :count)
+        expect(Committee.count).to eq(1)
       end
     end
   end
