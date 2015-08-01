555c5b790d7ad7e7fdf303fc4aa4e3f2e2faa036
Zack Youngren <zack@youngren.io>
2015-08-01 16:00:48 -0400

    Write association specs for player model
    * Change existing validation specs to use shoulda matchers

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,8 +2,8 @@ class Player < ActiveRecord::Base
   belongs_to :member
   has_one :team, through: :team_players
 
-  validates :name, presence: true, uniqueness: { scope: :number,
-    case_sensitive: false}
+  validates :name, presence: true,
+                   uniqueness: { scope: :number , case_sensitive: false}
   validates :number, presence: true,
                      format: { with: /\A(?=[a-zA-Z]*\d)(?=[a-zA-Z]*).{1,4}\z/ }
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,64 +1,35 @@
 describe Player do
-  describe 'validation' do
-    context 'is valid' do
-      it 'with a name and number' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Schneewittchen', number: '1')
-        Player.create(name: 'Blick', number: '1A')
-        Player.create(name: 'Flick', number: 'A1')
-        Player.create(name: 'Glick', number: 'A1A')
-        Player.create(name: 'Plick', number: '1111')
-        Player.create(name: 'Quee', number: 'AAA1')
-        Player.create(name: 'Snick', number: '1AAA')
-        Player.create(name: 'Whick', number: '1AA1')
-        expect(Player.count).to eq(8)
-      end
+  describe 'associations' do
+    it 'should belong to member' do
+      should belong_to(:member)
+    end
+  end
+
+  describe 'validations' do
+    # Because of a weird corner case in the should libary
+    subject { Player.new(name: 'Myra', number: '1') }
+
+    it 'should validate the presence of a name' do
+      should validate_presence_of(:name)
     end
 
-    context 'is invalid' do
-      it 'without a name' do
-        expect(Player.count).to eq(0)
-        Player.create(number: '1')
-        expect(Player.count).to eq(0)
-      end
+    it 'should validate uniqueness of name' do
+      should validate_uniqueness_of(:name).scoped_to(:number).case_insensitive
+    end
 
-      it 'without a number' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Kabo')
-        expect(Player.count).to eq(0)
-      end
-
-      it 'with a number too short' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Vint', number: '')
-        expect(Player.count).to eq(0)
-      end
-
-      it 'with a number too long' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Belle', number: 'AB123')
-        expect(Player.count).to eq(0)
-      end
-
-      it 'with a number with illegal characters' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Teryn', number: '-4')
-        Player.create(name: 'Velly', number: '.45')
-        expect(Player.count).to eq(0)
-      end
-
-      it 'with a digitless number' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Darren', number: 'ABC')
-        expect(Player.count).to eq(0)
-      end
-
-      it 'with a duplicate name and number' do
-        expect(Player.count).to eq(0)
-        Player.create(name: 'Red', number: '12')
-        Player.create(name: 'Red', number: '12')
-        expect(Player.count).to eq(1)
-      end
+    it 'should validate the format of the number' do
+      should allow_value('0').for(:number)
+      should allow_value('0A').for(:number)
+      should allow_value('A0').for(:number)
+      should allow_value('0A00').for(:number)
+      should allow_value('AAA0').for(:number)
+      should allow_value('0AAA').for(:number)
+      should_not allow_value('').for(:number)
+      should_not allow_value('A').for(:number)
+      should_not allow_value('AAAA').for(:number)
+      should_not allow_value('AAAAA').for(:number)
+      should_not allow_value('AAAA1').for(:number)
+      should_not allow_value('11111').for(:number)
     end
   end
 end
