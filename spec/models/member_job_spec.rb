fe168746f9bd858c79f32322822b923f79aef623
Zack Youngren <zack@youngren.io>
2015-08-01 14:58:02 -0400

    Write association specs for member_job
    * Move validation tests to shoulda matchers

--- spec/models/member_job_spec.rb
+++ spec/models/member_job_spec.rb
@@ -24,5 +24,48 @@ describe MemberJob do
     it 'should validate the presence of member_id' do
       should validate_presence_of(:member_id)
     end
+
+    # Shoulda can't test this
+
+    it 'should fail with a string for start_date' do
+      expect(MemberJob.count).to eq(0)
+      MemberJob.create(member_id: 1,
+                       job_id: 1,
+                       date_started: 'string')
+      expect(MemberJob.count).to eq(0)
+    end
+
+    it 'should fail with a number for start_date' do
+      expect(MemberJob.count).to eq(0)
+      MemberJob.create(member_id: 1,
+                       job_id: 1,
+                       date_started: 123)
+      expect(MemberJob.count).to eq(0)
+    end
+
+    it 'should fail with a string for end_Date' do
+      expect(MemberJob.count).to eq(0)
+      MemberJob.create(member_id: 1,
+                       job_id: 1,
+                       date_ended: 'string')
+      expect(MemberJob.count).to eq(0)
+    end
+
+    it 'should fail with a number for end_date' do
+      expect(MemberJob.count).to eq(0)
+      MemberJob.create(member_id: 1,
+                       job_id: 1,
+                       date_ended: 123)
+      expect(MemberJob.count).to eq(0)
+    end
+
+    it 'should fail with an end date before a start date' do
+      expect(MemberJob.count).to eq(0)
+      MemberJob.create(member_id: 1,
+                       job_id: 1,
+                       date_started: Date.today,
+                       date_ended: Date.today - 1)
+      expect(MemberJob.count).to eq(0)
+    end
   end
 end
