d476355583aafa67787e338f66bfb5db9ef5f677
Zack Youngren <zack@youngren.io>
2015-08-01 15:07:44 -0400

    Write association tests for committee_member
    * Move existing validation specs to shoulda matchers

--- spec/models/committee_member_spec.rb
+++ spec/models/committee_member_spec.rb
@@ -1,80 +1,69 @@
 describe CommitteeMember do
-  describe 'validation' do
-    context 'is valid' do
-      it 'with duplicate member_id' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(committee_id: 1, member_id: 1)
-        CommitteeMember.create(committee_id: 2, member_id: 1)
-        expect(CommitteeMember.count).to eq(2)
-      end
-
-      it 'with duplicate committee' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(committee_id: 1, member_id: 1)
-        CommitteeMember.create(committee_id: 1, member_id: 2)
-        expect(CommitteeMember.count).to eq(2)
-      end
-
-      it 'with no start date or end date' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(member_id: 1, committee_id: 1)
-        expect(CommitteeMember.count).to eq(1)
-      end
-
-      it 'with a start date and no end date' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(member_id: 1,
-                               committee_id: 1,
-                               date_started: Date.today)
-        expect(CommitteeMember.count).to eq(1)
-      end
-
-      it 'with an end date and no start date' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(member_id: 1,
-                               committee_id: 1,
-                               date_ended: Date.today)
-        expect(CommitteeMember.count).to eq(1)
-      end
-
-      it 'with an end date after a start date' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(member_id: 1,
-                               committee_id: 1,
-                               date_started: Date.today - 1,
-                               date_ended: Date.today)
-        expect(CommitteeMember.count).to eq(1)
-      end
+  describe 'associations' do
+    it 'should belong to committee' do
+      should belong_to(:committee)
     end
 
-    context 'is invalid' do
-      it 'with duplicate member_id and committee_id' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(committee_id: 1, member_id: 1)
-        CommitteeMember.create(committee_id: 1, member_id: 1)
-        expect(CommitteeMember.count).to eq(1)
-      end
+    it 'should belong_to member' do
+      should belong_to(:member)
+    end
+  end
 
-      it 'without a member_id' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(committee_id: 1)
-        expect(CommitteeMember.count).to eq(0)
-      end
+  describe 'validations' do
+    # Because of a weird corner case in the should libary
+    subject { CommitteeMember.new(committee_id: 1, member_id: 1) }
 
-      it 'without a committee_id' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(member_id: 1)
-        expect(CommitteeMember.count).to eq(0)
-      end
+    it 'should validate the presence of a committee id' do
+      should validate_presence_of(:committee_id)
+    end
 
-      it 'with an end date before a start date' do
-        expect(CommitteeMember.count).to eq(0)
-        CommitteeMember.create(member_id: 1,
-                               committee_id: 1,
-                               date_started: Date.today,
-                               date_ended: Date.today - 1)
-        expect(CommitteeMember.count).to eq(0)
-      end
+    it 'should validate the presence of a member_id' do
+      should validate_presence_of(:member_id)
+    end
+
+    it 'should validate the uniqueness of the committee and member ids' do
+      should validate_uniqueness_of(:member_id).scoped_to(:committee_id)
+    end
+
+    # Should can't test these
+    it 'should fail with a string for an start_date' do
+      expect(CommitteeMember.count).to eq(0)
+      CommitteeMember.create(member_id: 1,
+                             committee_id: 1,
+                             date_started: 'string')
+      expect(CommitteeMember.count).to eq(0)
+    end
+
+    it 'should fail with a number for an start_date' do
+      expect(CommitteeMember.count).to eq(0)
+      CommitteeMember.create(member_id: 1,
+                             committee_id: 1,
+                             date_started: 123)
+      expect(CommitteeMember.count).to eq(0)
+    end
+    it 'should fail with a string for an end_date' do
+      expect(CommitteeMember.count).to eq(0)
+      CommitteeMember.create(member_id: 1,
+                             committee_id: 1,
+                             date_ended: 'string')
+      expect(CommitteeMember.count).to eq(0)
+    end
+
+    it 'should fail with a number for an end_date' do
+      expect(CommitteeMember.count).to eq(0)
+      CommitteeMember.create(member_id: 1,
+                             committee_id: 1,
+                             date_ended: 123)
+      expect(CommitteeMember.count).to eq(0)
+    end
+
+    it 'should fail with an end date before a start date' do
+      expect(CommitteeMember.count).to eq(0)
+      CommitteeMember.create(member_id: 1,
+                             committee_id: 1,
+                             date_started: Date.today,
+                             date_ended: Date.today - 1)
+      expect(CommitteeMember.count).to eq(0)
     end
   end
 end
