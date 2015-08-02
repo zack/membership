ce8fae54200398ee5fa10cd22c36700521cca3d4
Zack Youngren <zack@youngren.io>
2015-08-02 10:36:27 -0400

    Write association specs for team
    * Refactor all validation specs to use shoulda matchers
    * Validate date_started for team model
    * Correct the team model associations

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,9 @@
 class Team < ActiveRecord::Base
+  has_many :team_players
   has_many :players, through: :team_players
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  validates_date :date_started
   validates_date :date_ended, :after => :date_started,
                               :allow_nil => true,
                               :if => :date_started
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,54 +1,58 @@
 describe Team do
-  describe 'validation' do
-    context 'is valid' do
-      it 'with a name' do # and no date
-        expect(Team.count).to eq(0)
-        Team.create(name: 'Massacre')
-        expect(Team.count).to eq(1)
-      end
-
-      it 'with an end date and no start date' do
-        expect(Team.count).to eq(0)
-        Team.create(name: 'Massacre', date_ended: Date.today)
-        expect(Team.count).to eq(1)
-      end
-
-      it 'with an end date after a start date' do
-        expect(Team.count).to eq(0)
-        Team.create(name: 'Massacre',
-                    date_started: Date.today - 1,
-                    date_ended: Date.today)
-        expect(Team.count).to eq(1)
-      end
+  describe 'associations' do
+    it 'have many team_players' do
+      should have_many(:team_players)
     end
 
-    context 'is invalid' do
-      it 'without a name' do
-        expect(Team.count).to eq(0)
-        Team.create()
-        expect(Team.count).to eq(0)
-      end
+    it 'have many players through team players' do
+      should have_many(:players).through(:team_players)
+    end
+  end
+  describe 'validations' do
+    it 'should validate the presence of a name' do
+      should validate_presence_of(:name)
+    end
 
-      it 'with a duplicate name' do
-        expect(Team.count).to eq(0)
-        Team.create(name: 'Massacre')
-        Team.create(name: 'Massacre')
-        expect(Team.count).to eq(1)
-      end
+    it 'should validate the uniqueness of a name' do
+      should validate_uniqueness_of(:name).case_insensitive
+    end
 
-      it 'with a duplicate name regardless of case' do
-        expect(Team.count).to eq(0)
-        Team.create(name: 'massacre')
-        Team.create(name: 'MASSACRE')
-        expect(Team.count).to eq(1)
-      end
+    # Shoulda can't test these
 
-      it 'with an end date before a start date' do
-        Team.create(name: 'Massacre',
-                    date_started: Date.today,
-                    date_ended: Date.today - 1)
-        expect(Team.count).to eq(0)
-      end
+    it 'should fail with a string for an start_date' do
+      expect(Team.count).to eq(0)
+      Team.create(name: 'Toasters',
+                  date_started: 'string')
+      expect(Team.count).to eq(0)
+    end
+
+    it 'should fail with a number for an start_date' do
+      expect(Team.count).to eq(0)
+      Team.create(name: 'Toasters',
+                  date_started: 123)
+      expect(Team.count).to eq(0)
+    end
+
+    it 'should fail with a string for an end_date' do
+      expect(Team.count).to eq(0)
+      Team.create(name: 'Toasters',
+                  date_ended: 'string')
+      expect(Team.count).to eq(0)
+    end
+
+    it 'should fail with a number for an end_date' do
+      expect(Team.count).to eq(0)
+      Team.create(name: 'Toasters',
+                  date_ended: 123)
+      expect(Team.count).to eq(0)
+    end
+
+    it 'should fail with an end date before a start date' do
+      expect(Team.count).to eq(0)
+      Team.create(name: 'Toasters',
+                  date_started: Date.today,
+                  date_ended: Date.today - 1)
+      expect(Team.count).to eq(0)
     end
   end
 end
