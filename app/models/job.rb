6687afb80c5f75214fdaad8c4b12963993af3774
Zack Youngren <zack@youngren.io>
2015-08-02 08:47:04 -0400

    Write association specs for job
    * Add date_started date validation on the job model
    * Correct the job to member_job associations
    * Refactor all existing validation specs to use shoulda matchers

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,10 @@
 class Job < ActiveRecord::Base
   belongs_to :committee
-  has_one :member_job
-  has_one :member, through: :member_jobs
+  has_many :member_jobs
+  has_many :members, through: :member_jobs
 
   validates :name, presence: true
+  validates_date :date_started
   validates_date :date_ended, :after => :date_started,
                               :allow_nil => true,
                               :if => :date_started
--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,49 +1,59 @@
 describe Job do
-  describe 'validation' do
-    context 'is valid' do
-      it 'with no start date or end date' do
-        expect(Job.count).to eq(0)
-        Job.create(name: 'Rosaline')
-        expect(Job.count).to eq(1)
-      end
-
-      it 'with a start date and no end date' do
-        expect(Job.count).to eq(0)
-        Job.create(name: 'Rosaline',
-                   date_started: Date.today)
-        expect(Job.count).to eq(1)
-      end
-
-      it 'with an end date and no start date' do
-        expect(Job.count).to eq(0)
-        Job.create(name: 'Rosaline',
-                   date_started: Date.today)
-        expect(Job.count).to eq(1)
-      end
-
-      it 'with an end date after a start date' do
-        expect(Job.count).to eq(0)
-        Job.create(name: 'Rosaline',
-                   date_started: Date.today - 1,
-                   date_ended: Date.today)
-        expect(Job.count).to eq(1)
-      end
-
+  describe 'associations' do
+    it 'belongs to committee' do
+      should belong_to(:committee)
     end
-    context 'is invalid' do
-      it 'without a name' do
-        expect(Job.count).to eq(0)
-        Job.create()
-        expect(Job.count).to eq(0)
-      end
 
-      it 'with an end date before a start date' do
-        expect(Job.count).to eq(0)
-        Job.create(name: 'Rosaline',
-                   date_started: Date.today,
-                   date_ended: Date.today - 1)
-        expect(Job.count).to eq(0)
-      end
+    it 'has many member_job' do
+      should have_many(:member_jobs)
+    end
+
+    it 'has many members through member_jobs' do
+      should have_many(:members).through(:member_jobs)
+    end
+  end
+
+  describe 'validations' do
+    it 'should valicate the presence of a name' do
+      should validate_presence_of(:name)
+    end
+
+    # Shoulda can't test this
+
+    it 'should fail with a string for start_date' do
+      expect(Job.count).to eq(0)
+      Job.create(name: 'President',
+                 date_started: 'string')
+      expect(Job.count).to eq(0)
+    end
+
+    it 'should fail with a number for start_date' do
+      expect(Job.count).to eq(0)
+      Job.create(name: 'President',
+                 date_started: 123)
+      expect(Job.count).to eq(0)
+    end
+
+    it 'should fail with a string for end_Date' do
+      expect(Job.count).to eq(0)
+      Job.create(name: 'President',
+                 date_ended: 'string')
+      expect(Job.count).to eq(0)
+    end
+
+    it 'should fail with a number for end_date' do
+      expect(Job.count).to eq(0)
+      Job.create(name: 'President',
+                 date_ended: 123)
+      expect(Job.count).to eq(0)
+    end
+
+    it 'should fail with an end date before a start date' do
+      expect(Job.count).to eq(0)
+      Job.create(name: 'President',
+                 date_started: Date.today,
+                 date_ended: Date.today - 1)
+      expect(Job.count).to eq(0)
     end
   end
 end
