f4d45293488b4dbf3604f5ed6b715d4f078b5f15
Zack Youngren <zack@youngren.io>
2015-08-27 23:52:51 -0400

    rails generate backbone:install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
+//= require underscore
+//= require backbone
+//= require membership
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ app/assets/javascripts/membership.js.coffee
@@ -0,0 +1,9 @@
+window.Membership =
+  Models: {}
+  Collections: {}
+  Views: {}
+  Routers: {}
+  initialize: -> alert 'Hello from Backbone!'
+
+$(document).ready ->
+  Membership.initialize()
